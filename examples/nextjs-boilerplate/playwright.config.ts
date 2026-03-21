cf6e6f56d85a602ed9dd59a73bf24e70f851d4d9
martinriotorto <martin@artificialmente.uy>
2026-03-21 17:42:44 -0300

    chore: remove external bitmovin HLS e2e test fixture to ensure 100% locally deterministic testing

--- examples/nextjs-boilerplate/playwright.config.ts
+++ examples/nextjs-boilerplate/playwright.config.ts
@@ -14,7 +14,12 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { 
+        ...devices['Desktop Chrome'],
+        launchOptions: {
+          args: ['--autoplay-policy=no-user-gesture-required']
+        }
+      },
     },
   ],
 });
